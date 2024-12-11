6c9adadea0f2ce53a5a11751cf40907b1e1ba588
sudhidutta7694 <sudhi.dutta7694@gmail.com>
2024-12-11 10:06:51 +0530

    Basic Features Implemented

---
+++ client/client_main.cpp
@@ -0,0 +1,10 @@
+/***************headers start******************/
+#include "defs.hpp"
+
+/***************headers end********************/
+
+//main entry point to the client
+int main(){
+    RunClient();
+    return 0;
+}
